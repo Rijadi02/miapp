274d00a8657d7ce457cccc751fcd35011aa8d607
venismorina <venismorinax8@gmail.com>
2021-09-12 23:32:56 +0200

    recitimet

--- resources/views/recitations.blade.php
+++ resources/views/recitations.blade.php
@@ -40,24 +40,55 @@
                 <div class="row">
                     @foreach ($recitations as $recitation)
 
-                        <div class="col-lg-4">
-                            <div class="card mb-5" style="width: 18rem;">
-                                <a href="{{ route('reciter', $recitation->reciter->slug) }}">
-                                    <img class="card-img-top" src="/storage/{{ $recitation->reciter->image }}"
+                        {{-- <div class="col-lg-3">
+                            <a href="{{ route('reciter', $recitation->reciter->slug) }}">
+                                <div class="reciter__image">
+                                    <img src="/storage/{{ $recitation->reciter->image }}"
                                         alt="Card image cap">
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="card ">
+                            <h3 class="card-title">{{ $recitation->reciter->name }}</h3>
+
+                            <div class="card-body">
+
+                                <a href="{{ route('recitations.show', $recitation->id) }}">
+                                    <h5 class="card-title">{{ $recitation->title }}</h5>
                                 </a>
-                                <div class="card-body">
-                                    <h5 class="card-title">{{ $recitation->reciter->name }}</h5>
-                                    <a href="{{ route('recitations.show', $recitation->id) }}">
-                                        <h5 class="card-title">{{ $recitation->title }}</h5>
+                                <audio style="max-width: 100%;" controls>
+                                    <source src="/storage/{{ $recitation->audio }}" type="audio/mpeg">
+                                    Your browser does not support the audio element.
+                                </audio>
+                            </div>
+                        </div> --}}
+
+
+                                <div class="col-lg-2" >
+                                    <a href="{{ route('reciter', $recitation->reciter->slug) }}">
+                                        <div class="reciter__image">
+                                            <img src="/storage/{{ $recitation->reciter->image }}" alt="Card image cap"
+                                                style="max-width: 98%;">
+                                        </div>
                                     </a>
-                                    <audio style="max-width: 100%;" controls>
+                                </div>
+                                <div class="col-lg-10">
+                                    <a href="{{ route('recitations.show', $recitation->id) }}">
+                                        <h5 class="card-title mt-4 ms-3">{{ $recitation->title }}</h5>
+                                    </a>
+                                    <h3 class="card-title mt-3 ms-3">{{ $recitation->reciter->name }}</h3>
+                                </div>
+                                <div class="col-lg-12 mt-0 pt-0 mb-5" style="padding: 1%;">
+                                    <audio style="min-width: 100%;" class="mt-5" controls="">
                                         <source src="/storage/{{ $recitation->audio }}" type="audio/mpeg">
                                         Your browser does not support the audio element.
                                     </audio>
                                 </div>
-                            </div>
-                        </div>
+
+
+
+
 
                     @endforeach
                 </div>
