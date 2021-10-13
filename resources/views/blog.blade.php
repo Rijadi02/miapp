4036483e4a3d0cc18593e5cabc098c40c8c92415
Rijadi's Server <adiveni0@gmail.com>
2021-10-13 16:38:43 +0200

    share 2nd try

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -76,6 +76,21 @@
                             Share on Facebook
                         </a>
 
+                        <div id="fb-root"></div>
+                        <script>(function(d, s, id) {
+                        var js, fjs = d.getElementsByTagName(s)[0];
+                        if (d.getElementById(id)) return;
+                        js = d.createElement(s); js.id = id;
+                        js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+                        fjs.parentNode.insertBefore(js, fjs);
+                        }(document, 'script', 'facebook-jssdk'));</script>
+
+                        <!-- Your share button code -->
+                        <div class="fb-share-button"
+                        data-href="http://mi.tachyonstudio.com/+ <?php echo url('/'); ?>/artikulli/{{ $blog->slug }}"
+                        data-layout="button_count">
+                        share aaaaaa
+                        </div>
 
                     </div>
                     {{-- <div class="news-details__bottom">
