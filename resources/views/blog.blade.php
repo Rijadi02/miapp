bf5cd08b89917b2748910f56d92cbb86c447b621
Rijadi's Server <adiveni0@gmail.com>
2021-10-14 10:38:06 +0200

    pe provojm a po bon bre

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -71,13 +71,12 @@
                     </div>
 
                     <?php $url = url('/')."/artikulli/".$blog->slug?>
-                    {{$url}}
                     <div class="news-details__bottom">
                         <div class="news-details__social-list">
-                            <a href="https://www.facebook.com/dialog/share?app_id=1266109583813461&display=popup&href=<?php echo url('/'); ?>/artikulli/{{ $blog->slug }}&redirect_uri=<?php echo url('/'); ?>/artikulli/{{ $blog->slug }}" target="_blank"><i class="fab fa-facebook"></i></a>
-                            <a href="fb-messenger://share/?link=<?php echo url('/'); ?>/artikulli/{{ $blog->slug }}&redirect_uri=<?php echo url('/'); ?>/artikulli/{{ $blog->slug }}&app_id=1266109583813461"><i class="fab fa-facebook-messenger"></i></a>
-                            <a href="whatsapp://send?text=<?php echo url('/'); ?>/artikulli/{{ $blog->slug }}&redirect_uri=<?php echo url('/'); ?>/artikulli/{{ $blog->slug }}" data-action="share/whatsapp/share" target="_blank"><i class="fab fa-whatsapp"></i></a>
-                            <a href="https://telegram.me/share/url?url=<?php echo url('/'); ?>/artikulli/{{ $blog->slug }}&redirect_uri=<?php echo url('/'); ?>/artikulli/{{ $blog->slug }}&text={{ $blog->title }}" target="_blank"><i class="fab fa-telegram-plane"></i></a>
+                            <a href="https://www.facebook.com/dialog/share?app_id=1266109583813461&display=popup&href=<?php echo $url ?>&redirect_uri=<?php echo $url ?>" target="_blank"><i class="fab fa-facebook"></i></a>
+                            <a href="fb-messenger://share/?link=<?php echo $url ?>&redirect_uri=<?php echo $url ?>&app_id=1266109583813461"><i class="fab fa-facebook-messenger"></i></a>
+                            <a href="whatsapp://send?text=<?php echo $url ?>&redirect_uri=<?php echo $url ?>" data-action="share/whatsapp/share" target="_blank"><i class="fab fa-whatsapp"></i></a>
+                            <a href="https://telegram.me/share/url?url=<?php echo $url ?>&redirect_uri=<?php echo $url ?>" target="_blank"><i class="fab fa-telegram-plane"></i></a>
                         </div>
                     </div>
 
