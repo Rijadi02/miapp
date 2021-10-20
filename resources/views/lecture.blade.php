7a9f4732187932145283219b01ea9c6d34fd0ad2
Rijadi's Server <adiveni0@gmail.com>
2021-10-20 10:07:43 +0200

    lectures

--- resources/views/lecture.blade.php
+++ resources/views/lecture.blade.php
@@ -54,16 +54,32 @@
                                 <!--Popular Tours Two Single-->
                                 <div class="popular-tours__single">
                                     <div class="popular-tours__img">
+
+                                        <?php $url = url('/')."/dersi/".$lecture->id?>
+
+
                                         <img style="object-fit: cover;aspect-ratio:1" src="/storage/{{ $lecture->lecturer->image }}" alt="">
                                         <div class="popular-tours__icon">
-                                            <a href="tour-details.html">
-                                                <i class="fa fa-heart"></i>
+                                            <a href="https://www.facebook.com/dialog/share?app_id=1266109583813461&display=popup&href=<?php echo $url ?>&redirect_uri=<?php echo $url ?>" target="_blank">
+                                                <i class="fab fa-facebook"></i>
+                                            </a>
+                                            <a class="d-none-mobile" href="fb-messenger://share/?link=<?php echo $url ?>&redirect_uri=<?php echo $url ?>&app_id=1266109583813461" target="_blank">
+                                                <i class="fab fa-facebook-messenger"></i>
+                                            </a>
+                                            <a href="whatsapp://send?text=<?php echo $url ?>&redirect_uri=<?php echo $url ?>" data-action="share/whatsapp/share" target="_blank">
+                                                <i class="fab fa-whatsapp"></i>
+                                            </a>
+                                            <a href="https://telegram.me/share/url?url=<?php echo $url ?>&redirect_uri=<?php echo $url ?>" target="_blank">
+                                                <i class="fab fa-telegram-plane"></i>
+                                            </a>
+                                            <a href="javascript:copy_text('<?php echo $url ?>');">
+                                                <i class="fas fa-copy"></i>
                                             </a>
                                         </div>
                                     </div>
                                     <div class="popular-tours__content">
 
-                                        <h3 class="popular-tours__title"><a href="tour-details.html">{{ $lecture->title }}
+                                        <h3 class="popular-tours__title"><a href="{{route('lecture',$lecture->id)}}">{{ $lecture->title }}
                                             </a></h3>
                                         <ul class="footer-widget__about-contact list-unstyled">
                                             <li>
@@ -71,7 +87,7 @@
                                                     <i class="fas fa-map"></i>
                                                 </div>
                                                 <div class="mx-2">
-                                                    <a href="tel:+92-666-888-0000">{{ $lecture->place }}</a>
+                                                    <a href="{{ $lecture->map }}">{{ $lecture->place }}</a>
                                                 </div>
                                             </li>
                                             <li>
@@ -79,7 +95,7 @@
                                                     <i class="fas fa-clock"></i>
                                                 </div>
                                                 <div class="mx-2">
-                                                    <a href="tel:+92-666-888-0000">{{ $lecture->time }}</a>
+                                                    <a >{{ $lecture->time }}</a>
                                                 </div>
 
                                             </li>
@@ -89,15 +105,15 @@
                                                     <i class="fas fa-users"></i>
                                                 </div>
                                                 <div class="mx-2">
-                                                    <a href="tel:+92-666-888-0000">{{ $lecture->allowance }}</a>
+                                                    <a>{{ $lecture->allowance }}</a>
                                                 </div>
 
                                             </li>
 
                                         </ul>
                                         <div class="d-flex mt-3">
-                                            <a class="ders ders{{ $lecture->status == "1" ? "--active" : "" }} " href="tour-details.html">{{ $lecture->status == "1" ? "Mbahet" : "Nuk Mbahet" }}</a>
-                                            <a style="font-size: 15px" class="ms-3" href="tour-details.html">{{ $lecture->date }}</a>
+                                            <a class="ders ders{{ $lecture->status == "1" ? "--active" : "" }} " >{{ $lecture->status == "1" ? "Mbahet" : "Nuk Mbahet" }}</a>
+                                            <a style="font-size: 15px" class="ms-3">{{ $lecture->date }}</a>
 
                                         </div>
 
