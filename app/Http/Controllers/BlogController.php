611b286dc6a5f0a11159d10a6f72eb294b60ef64
Rijadi's Server <adiveni0@gmail.com>
2021-12-30 10:46:11 +0100

    get articles

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -14,7 +14,7 @@ class BlogController extends Controller
      */
     public function index()
     {
-        $blogs = Blog::orderBy('created_at', 'desc');
+        $blogs = Blog::orderBy('created_at', 'desc')->get();
         return view('admin/blog', compact('blogs'));
     }
 
