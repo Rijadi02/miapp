70cc0dd924809696cf85e4ae4ebcca12c7c5f914
venismorina <venismorinax8@gmail.com>
2022-03-15 15:42:17 +0100

    category

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -54,7 +54,7 @@
             window.onload = (event) => {
                 if (isMobile()) {
                     window.location =
-                        "miapp://articles/article?slug={{ $blog->slug }}&{{ $blog->slug }}&title={{ $blog->title }}&author={{ $blog->author }}&image={{ $blog->image }}&date={{ $blog->updated_at->format('Y-m-d') }}";
+                        "miapp://articles/article?slug={{ $blog->slug }}&title={{ $blog->title }}&author={{ $blog->author }}&image={{ $blog->image }}&date={{ $blog->updated_at->format('Y-m-d') }}&category={{ $blog->category }}&redirect=true";
 
                 }
             };
