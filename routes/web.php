12ea0ed1a2430aaa804f230af809fcf790d84979
Rijadi02 <adiveni0@gmail.com>
2023-04-03 10:38:02 +0200

    dita 12

--- routes/web.php
+++ routes/web.php
@@ -116,6 +116,10 @@ Route::get("/planifikuesi/dersi/11", function () {
     return redirect('https://youtu.be/9SFcY6CLoOM');
 });
 
+Route::get("/planifikuesi/dersi/12", function () {
+    return redirect('https://youtu.be/BCNJFm1DnxI');
+});
+
 Route::group(['namespace' => 'Admin', 'middleware' => ['auth'], 'prefix' => 'admin'], function () {
 
     Route::get('categories', [App\Http\Controllers\CategoryController::class, 'index'])->name('category.index');
